aadfbc9e7ad751b73efa8ecc984d6b1d8206871b
prog-pog <106827047+prog-pog@users.noreply.github.com>
2024-03-08 20:35:15 +0000

    Finised 'diceGames'

--- pythonG/diceGames/main.py
+++ pythonG/diceGames/main.py
@@ -1,17 +1,45 @@
+import moneyhand as mh
+
+
 def main():
     import dice
     import moneyhand as mh
-    import json
 
 
-    money = 0
+    data = mh.load()
+    money = data["money"]
     dice.clear()
     q = dice.Dice()
-    q.settings(10, 1)
-    q.set(4)
+    q.settings(data["delay"], data["mult"])
+    q.set(data["sides"])
     rol = q.roll()
     money = money + rol
-    mh.save(q)
+    print("{}$".format(money))
+    save = {"delay" : data["delay"], "mult": data["mult"], "sides": data["sides"], "money": money}
+    mh.save(save)
 
 if __name__ == "__main__":
-    main()
+    while True:
+        try:
+            main()
+            c = input("Options, [ENTER] = Continue, [M] = Shop, [E] = Exit\n")
+            if c.capitalize() == "M":
+                data = mh.load()
+                o = input("1. Side = {0}$\n2. Mult = {1}$\n".format(int(data["sides"])**2, int(data["mult"])**3))
+                if o == "1" and data["money"] >= int(data["sides"])**2:
+                    money = data["money"] - int(data["sides"])**2
+                    sides = int(data["sides"]) + 1
+                    save = {"delay" : data["delay"], "mult": data["mult"], "sides": sides, "money": money}
+                    mh.save(save)
+                if o == "2" and data["money"] >= int(data["mult"])**3:
+                    money = data["money"] - int(data["mult"])**3
+                    mult = int(data["mult"]) + 1
+                    save = {"delay" : data["delay"], "mult": mult, "sides": data["sides"], "money": money}
+                    mh.save(save)
+                else:
+                    pass
+            if c.capitalize() == "E":
+                exit("")
+            else: pass
+        except KeyboardInterrupt:
+            exit("\nOk, you ended it")
--- pythonG/diceGames/moneyhand.py
+++ pythonG/diceGames/moneyhand.py
@@ -1,18 +1,27 @@
 import json
+import base64
 
 
 def save(dice):
-    data = json.dumps(dice.__dict__, indent=4)
-    with open("save.json", "w") as file:
+    data = json.dumps(dice, indent=4)
+    with open("pythonG/diceGames/save.json", "w") as file:
         file.flush()
         file.write(data)
     file.close()
 
 def load():
     try:
-        with open("save.json", "r") as file:
-            data = file.read()
+        with open("pythonG/diceGames/save.json", "r") as file:
+            data = json.loads(file.read())
         file.close()
-        print(data)
     except FileNotFoundError:
-        exit()
+        with open("pythonG/diceGames/save.json", "w") as file:
+            file.flush()
+            data = {"delay": 50, "mult": 1, "sides": 2, "money": 0}
+            data = json.dumps(data, indent=4)
+            file.write(data)
+        file.close()
+        assert True
+    except:
+        raise
+    return data
