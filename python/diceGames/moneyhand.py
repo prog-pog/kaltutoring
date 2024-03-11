39b62a210c5451f48697795ca32c89ebe5d7b647
prog-pog <106827047+prog-pog@users.noreply.github.com>
2024-03-11 18:25:01 +0000

    main

--- python/diceGames/moneyhand.py
+++ python/diceGames/moneyhand.py
@@ -3,25 +3,34 @@ import base64
 
 
 def save(dice):
-    data = json.dumps(dice, indent=4)
-    with open("pythonG/diceGames/save.json", "w") as file:
+    data = json.dumps(dice)
+    data = data.encode()
+    data = base64.b64encode(data)
+    with open("python/diceGames/save.json", "w") as file:
         file.flush()
+        data = data.decode()
         file.write(data)
     file.close()
 
 def load():
     try:
-        with open("pythonG/diceGames/save.json", "r") as file:
-            data = json.loads(file.read())
+        with open("python/diceGames/save.json", "r") as file:
+            data = base64.b64decode(file.read())
+            print(data)
+            data = data.decode()
+            print(data)
+            data = json.loads(data)
+            print(type(data))
         file.close()
     except FileNotFoundError:
-        with open("pythonG/diceGames/save.json", "w") as file:
+        with open("python/diceGames/save.json", "w") as file:
             file.flush()
             data = {"delay": 20, "mult": 1, "sides": 2, "money": 0}
-            data = json.dumps(data, indent=4)
+            data = json.dumps(data)
+            data = data.encode()
+            data = base64.b64encode(data)
             file.write(data)
         file.close()
-        assert True
     except:
         raise
     return data
