0fe8fb08a1a71119bbfc141542ef4c40241b4184
prog-pog <106827047+prog-pog@users.noreply.github.com>
2024-03-11 19:21:59 +0000

    Added an encoding system for 'python/diceGames'

--- python/diceGames/moneyhand.py
+++ python/diceGames/moneyhand.py
@@ -6,6 +6,8 @@ def save(dice):
     data = json.dumps(dice)
     data = data.encode()
     data = base64.b64encode(data)
+    data = base64.b85encode(data)
+    data = base64.b64encode(data)
     with open("python/diceGames/save.txt", "w") as file:
         file.flush()
         data = data.decode()
@@ -16,11 +18,10 @@ def load():
     try:
         with open("python/diceGames/save.txt", "r") as file:
             data = base64.b64decode(file.read())
-            print(data)
+            data = base64.b85decode(data)
+            data = base64.b64decode(data)
             data = data.decode()
-            print(data)
             data = json.loads(data)
-            print(type(data))
         file.close()
     except FileNotFoundError:
         with open("python/diceGames/save.txt", "w") as file:
@@ -28,6 +29,8 @@ def load():
             data = {"delay": 20, "mult": 1, "sides": 2, "money": 0}
             data = json.dumps(data)
             data = data.encode()
+            data = base64.b64encode(data)
+            data = base64.b85encode(data)
             data = base64.b64encode(data)
             file.write(data)
         file.close()
