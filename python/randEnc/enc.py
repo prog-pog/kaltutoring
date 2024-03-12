8a3780064eba8a89cc506d646a46d7806d5676e7
prog-pog <106827047+prog-pog@users.noreply.github.com>
2024-03-12 20:06:03 +0000

    main

--- python/randEnc/enc.py
+++ python/randEnc/enc.py
@@ -3,23 +3,22 @@ import random
 import json
 
 
-def e(tx):
-    ch = []
-    saveto = open("python/randEnc/text.txt", "w")
-    saveto.flush()
-    amm = random.randint(2, 4)
-    print(amm)
-    text = str(tx)
-    text = text.encode()
-    for i in range(amm):
-        cho = random.randint(0, 1)
-        ch.append(cho)
-    print(ch)
-    for i in range(amm):
-        if ch[i] == "1":
-            text = base64.b85encode(text)
-        else:
-            text = base64.b64encode(text)
-    save = json.dumps(ch)
-    saveto.write(save)
-    saveto.write(text.decode())
+ch = []
+saveto = open("python/randEnc/text.txt", "w")
+saveto.flush()
+amm = random.randint(2, 4)
+print(amm)
+text = input("What do you want to convert?")
+text = text.encode()
+for i in range(amm):
+    cho = random.randint(0, 1)
+    ch.append(cho)
+print(ch)
+for i in range(amm):
+    if ch[i] == "1":
+        text = base64.b85encode(text)
+    else:
+        text = base64.b64encode(text)
+save = json.dumps(ch)
+saveto.write(save)
+saveto.write(text.decode())
