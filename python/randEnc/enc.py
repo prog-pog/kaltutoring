729edfdd7b829001ca3060c6388a296f4ccc9647
prog-pog <106827047+prog-pog@users.noreply.github.com>
2024-03-12 20:07:20 +0000

    main

--- python/randEnc/enc.py
+++ python/randEnc/enc.py
@@ -7,13 +7,11 @@ ch = []
 saveto = open("python/randEnc/text.txt", "w")
 saveto.flush()
 amm = random.randint(2, 4)
-print(amm)
 text = input("What do you want to convert?")
 text = text.encode()
 for i in range(amm):
     cho = random.randint(0, 1)
     ch.append(cho)
-print(ch)
 for i in range(amm):
     if ch[i] == "1":
         text = base64.b85encode(text)
