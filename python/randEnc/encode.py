aff52aa78afb5ebe44cdf9ce7988e7aac0054abf
prog-pog <106827047+prog-pog@users.noreply.github.com>
2024-03-13 04:18:24 +0000

    main

--- python/randEnc/encode.py
+++ python/randEnc/encode.py
@@ -7,7 +7,7 @@ ch = []
 saveto = open("python/randEnc/text.txt", "w")
 saveto.flush()
 amm = random.randint(2, 4)
-text = input("What do you want to convert?")
+text = input("What do you want to secure?\n")
 text = text.encode()
 for i in range(amm):
     cho = random.randint(0, 1)
@@ -18,6 +18,7 @@ for i in range(amm):
     else:
         text = base64.b64encode(text)
 save = json.dumps(ch)
-saveto.write(save)
+save = base64.b32encode(save.encode())
+saveto.write(save.decode())
 saveto.write("\n")
 saveto.write(text.decode())
