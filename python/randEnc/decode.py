a68d736af9db0e0c4c7a3c283ab267e38b854e0c
prog-pog <106827047+prog-pog@users.noreply.github.com>
2024-03-13 04:25:50 +0000

    main

--- python/randEnc/decode.py
+++ python/randEnc/decode.py
@@ -5,14 +5,15 @@ import json
 try:
     readFrom = open("python/randEnc/file.txt", "r")
     s1 = readFrom.readlines()
-    txt = base64.b32decode(str(s1[0]).encode())
-    save = json.loads(txt.encode())
+    s11 = s1[0].replace("\n", "")
+    txt = base64.b32decode(s11)
+    save = json.loads(txt.decode())
     text = s1[1].encode()
     for i in range(len(save)):
         if save[i] == "1":
             text = base64.b85decode(text)
         else:
             text = base64.b64decode(text)
-    text = text.decode()
+    print(text.decode())
 except:
     raise
